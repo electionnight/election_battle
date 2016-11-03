3de9ceb4a55fa718f924f9a4f1b3e664f008d6d1
amax3002 <alexandremguy@gmil.com>
2016-11-03 16:54:26 -0400

    update

--- cross-class-repo-master/server/config.ru
+++
@@ -1,2 +0,0 @@
-require_relative "lib/app"
-run App
--- cross-class-repo-master/server/test/app_test.rb
+++
@@ -1,9 +0,0 @@
-require_relative "test_helper"
-
-class AppTest < Minitest::Test
-  include Rack::Test::Methods
-
-  def app
-    App
-  end
-end
