d8d4a186a92a922e387b6f60ce87b48930cdcef8
Navi Kaur <nkaur901@gmail.com>
2016-11-05 11:53:38 -0400

    created a function for user to submit form after creating a candidate

--- client/public/js/data.js
+++ client/public/js/data.js
@@ -1,6 +1,45 @@
 (function() {
   'use strict';
 
-    window.election = window.election || {};
+  window.election = window.election || {};
+
+  function postCandidate(aboutCandidates) {
+  window.election.postCandidate = postCandidate;
+
+    var candidateName = aboutCandidate.name
+    var avatar = aboutCandidate.image_url
+    var intelligence = aboutCandidate.intelligence
+    var charisma = aboutCandidate.charisma
+    var willpower = aboutCandidate.willpower
+    console.log(aboutCandidates);
+    // do ajax call, url, metho, data json
+
+    $.ajax({
+
+      url: '/candidates',
+      method: 'POST',
+      data: JSON.stringify({name: candidateName,
+            image_url: avatar,
+            intelligence: intelligence,
+            charisma: charisma,
+            willpower: willpower
+            campaigns-won:
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .done( function handleSuccess(data) {
+      console.log(data);
+    })
+    .fail (function handleFail(xhr, errorType) {
+      console.log(xhr);
+    });
+
+  });
+
+
+
+window.election.postCandidate = postCandidate;
 
 }());
--- client/public/js/ui.js
+++ client/public/js/ui.js
@@ -1,8 +1,37 @@
-(function() {
+ week(function() {
   'use strict';
 
   window.election = window.election || {};
 
+
+// $('.show-candidate-list')
+//   .on('click', function showCandidate (event) {
+//     event.preventDefault();
+//     $('.show-candidate-list').toggleClass('visible');
+//     console.log('worked');
+//   });
+//
+//   $('.show-create-candidates')
+//     .on('click', function createCandidate (event) {
+//       event.preventDefault();
+//       $('.show-create-candidates').toggleClass('visible');
+//       console.log('worked');
+//     });
+
+$('.create-a-candidate')
+  .on('submit', function createCandidates(eventObj) {
+    eventObj.preventDefault();
+    var aboutCandidate = {};
+    aboutCandidate.name = $('#name').val();
+    aboutCandidate.image_url = $('#image_url').val();
+    aboutCandidate.intelligence = $('#intelligence').val();
+    aboutCandidate.charisma = $('#charisma').val();
+    aboutCandidate.willpower = $('#willpower').val();
+    console.log('hello');
+    window.election.postCandidate(aboutCandidate);
+}
+
+window.election.createCandidates = createCandidates();
 
 
 
